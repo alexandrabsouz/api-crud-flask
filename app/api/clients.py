acd70de05d7d4267b64716dfb7625e80ea2480ae
Alexandra Souza <alexandrabsouz@gmail.com>
2021-09-29 19:17:49 -0300

    feat: POST signup client

--- app/api/clients.py
+++ app/api/clients.py
@@ -19,3 +19,25 @@ def get_clients():
 @token_auth.login_required
 def get_client(id):
     return jsonify(Client.query.get_or_404(id).to_dict())
+
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json() or {}
+    if 'zip_code' not in data['address'] or 'state' not in data['address'] or 'city' not in data['address'] or 'number' not in data['address']:
+        return bad_request('Must include zip_code, state, city and number')
+    if 'email' not in data or 'full_name' not in data or 'password' not in data:
+        return bad_request('Must include full_name, email and password')
+    if Client.query.filter_by(email=data['email']).first():
+        return bad_request('Please use another e-mail')
+    client = Client()
+    client.from_dict(data, new_user=True)
+    db.session.add(client)
+    db.session.commit()
+    address = Address(client_id=client.id, zip_code=data['address']['zip_code'], state=data['address']['state'], city=data['address']['city'], number=data['address']['number'])
+    db.session.add(address)
+    db.session.commit()
+    response = jsonify(client.to_dict_with_address())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_client', id=client.id)
+    return response
