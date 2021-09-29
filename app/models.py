e4a7d84e54c09adb8fa28ff5f4f4cd0990c602f5
Alexandra Souza <alexandrabsouz@gmail.com>
2021-09-29 18:56:30 -0300

    feat: create table products

--- app/models.py
+++ app/models.py
@@ -131,3 +131,32 @@ class Address(PaginatedAPIMixin, db.Model):
         for field in ["zip_code", "state", "city", "number"]:
             if field in data:
                 setattr(self, field, data[field])
+
+
+class Product(PaginatedAPIMixin, db.Model):
+
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60))
+    price = db.Column(db.Float)
+    description = db.Column(db.String(300))
+    img_url = db.Column(db.String(300))
+
+    def __repr__(self):
+        return "<Product: {}>".format(self.id)
+
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "description": self.description,
+            "img_url": self.img_url
+        }
+        return data
+
+    def from_dict(self, data):
+        for field in ["name", "price", "description", "img_url"]:
+            if field in data:
+                setattr(self, field, data[field])
