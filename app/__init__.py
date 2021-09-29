2680d1cc45fb965224e585ade52d1c83c2c3cd91
Alexandra Souza <alexandrabsouz@gmail.com>
2021-09-29 18:38:45 -0300

    feat: add configuration in __init__ app

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
+
+    from app.api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix="/api/v1/")
+
+    from app import views, models
+
+    return app
