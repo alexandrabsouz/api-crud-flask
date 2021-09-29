cfdcbb1f59de31f35ce681ca3307674a90e98761
Alexandra Souza <alexandrabsouz@gmail.com>
2021-09-29 18:51:02 -0300

    feat: create table clients

--- app/models.py
+++ app/models.py
@@ -30,3 +30,49 @@ class PaginatedAPIMixin(object):
             },
         }
         return data
+
+
+class Client(PaginatedAPIMixin, db.Model):
+
+    __tablename__ = "clients"
+
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(60), index=True)
+    email = db.Column(db.String(60), index=True, unique=True)
+    password = db.Column(db.String(128))
+    address = db.relationship("Address", backref="client", uselist=False)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode("utf-8")
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = Client.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
+
+    def to_dict(self):
+        data = {
+            "id": self.id,
+            "full_name": self.full_name,
+            "email": self.email
+        }
+        return data
