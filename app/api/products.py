47a53c5f08e4b99b41a78b73b83bd446f78caa0e
Alexandra Souza <alexandrabsouz@gmail.com>
2021-10-03 20:23:18 -0300

    feat: GET all products

---
+++ app/api/products.py
@@ -0,0 +1,15 @@
+from flask import jsonify, request, url_for, g, abort
+from app import db
+from app.models import Product
+from app.api import api
+from app.api.errors import bad_request
+from app.api.auth import token_auth
+
+
+@api.route('/products', methods=['GET'])
+@token_auth.login_required
+def get_products():
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = Product.to_collection_dict(Product.query, page, per_page, 'api.get_products')
+    return jsonify(data)
