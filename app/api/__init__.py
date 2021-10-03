63306f850958886100271577a378e174d5bfe702
Alexandra Souza <alexandrabsouz@gmail.com>
2021-10-03 20:30:02 -0300

    feat: delete addresses

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api = Blueprint("api", __name__)
 
-from . import auth, errors, tokens, clients, addresses, products
+from . import auth, errors, tokens, clients, products
