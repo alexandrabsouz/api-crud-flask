08e9f4f2851c3f0d67a1c4e4cc9049a8e355b2ab
Alexandra Souza <alexandrabsouz@gmail.com>
2021-10-03 20:24:39 -0300

    feat: POST add product

--- app/api/products.py
+++ app/api/products.py
@@ -20,3 +20,19 @@ def get_products():
 def get_product(id):
     return jsonify(Product.query.get_or_404(id).to_dict())
 
+
+@api.route('/product', methods=['POST'])
+@token_auth.login_required
+def create_product():
+    data = request.get_json() or {}
+    if 'name' not in data or 'price' not in data or 'description' not in data or 'img_url' not in data:
+        return bad_request('Must include name, price, description, img_url')
+    product = Product()
+    product.from_dict(data)
+    product.client_id = g.current_user.id
+    db.session.add(product)
+    db.session.commit()
+    response = jsonify(product.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_product', id=product.id)
+    return response
