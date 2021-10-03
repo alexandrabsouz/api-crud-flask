4bd5f1fbfe62c6c5ef645da44fa52271d8a4ba1f
Alexandra Souza <alexandrabsouz@gmail.com>
2021-10-03 20:23:53 -0300

    feat: GET by id product

--- app/api/products.py
+++ app/api/products.py
@@ -13,3 +13,10 @@ def get_products():
     per_page = min(request.args.get('per_page', 10, type=int), 100)
     data = Product.to_collection_dict(Product.query, page, per_page, 'api.get_products')
     return jsonify(data)
+
+
+@api.route('/products/<int:id>', methods=['GET'])
+@token_auth.login_required
+def get_product(id):
+    return jsonify(Product.query.get_or_404(id).to_dict())
+
